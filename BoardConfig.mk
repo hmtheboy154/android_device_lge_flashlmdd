4907ee3e5e0871de6f9ba89f71346131da4a42ca
SGCMarkus <markusornik@gmail.com>
2020-03-14 14:41:19 +0100

    flashlmdd: fix recovery mounts

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,6 +23,7 @@ TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Recovery
+TARGET_RECOVERY_WIPE := $(DEVICE_PATH)/recovery/recovery.wipe
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 90
 
 # SELinux
